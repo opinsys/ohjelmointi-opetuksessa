1a7b68deff727fa6ef8882df6ec4bec830c43649
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-01-21 16:55:29 +0200

    Yksinkertaisempi asentokytkin käyttöön

--- varoitus_kaatumisesta/varoitus_kaatumisesta.ino
+++ varoitus_kaatumisesta/varoitus_kaatumisesta.ino
@@ -1,20 +1,17 @@
-int kallistus1 = 0;
-int kallistus2 = 0;
+int asentokytkin = 0;
 
 void setup() 
 {
-  pinMode(2, INPUT);
-  pinMode(4, INPUT);
+  pinMode(10, INPUT);
 }
 
 void loop() 
 {
-  kallistus1 = digitalRead(2);
-  kallistus2 = digitalRead(4);
+  asentokytkin = digitalRead(10);
   
   delay(100);
   
-  if(kallistus1 == 0 || kallistus2 == 0) {
+  if(asentokytkin == 0 ) {
     for (int taajuus=415; taajuus <= 615; taajuus++) {
       tone(12, taajuus);
       delay(5);
