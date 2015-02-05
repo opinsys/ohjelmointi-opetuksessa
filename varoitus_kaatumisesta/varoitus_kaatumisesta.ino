7225c14960f65b8c14d77ffe4add21cc90e2da57
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2015-02-05 07:35:36 +0200

    Korjaus kommentteihin varoitus kaatumisesta esimerkissä

--- varoitus_kaatumisesta/varoitus_kaatumisesta.ino
+++ varoitus_kaatumisesta/varoitus_kaatumisesta.ino
@@ -18,11 +18,14 @@ void loop()
   delay(100);                    // Odotetaan 100 millisekuntia
   
   if(asentokytkin == 0 ) {       // Mikäli asentokytkimen tila on 0 käynnistetään sireeni
-    // Seuraava toistolause käy läpi numerot 415:sta 614:sta yksi kerrallaan.
+
+    // Seuraava toistolause käy läpi numerot 415:sta 615:sta yksi kerrallaan.
     for (int taajuus=415; taajuus <= 615; taajuus++) {
       tone(12, taajuus); // Soitetaan ääni, jonka 'taajuus' muuttuja sisältää
       delay(5);          // Odotetaan 5 millisekuntia
     }
+
+    // Seuraava toistolause käy läpi numerot 615:sta 415:sta yksi kerrallaan.
     for (int taajuus=615; taajuus > 415; taajuus--) {
       tone(12, taajuus); // Soitetaan ääni, jonka 'taajuus' muuttuja sisältää
       delay(5);          // Odotetaan 5 millisekuntia
