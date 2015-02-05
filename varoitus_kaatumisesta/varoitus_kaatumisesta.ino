8ef5a3e6578537e37c549c334c338fb5a6706789
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2015-02-05 07:32:22 +0200

    Kommentoitiin varoitus kaatumisesta -esimerkkiohjelma

--- varoitus_kaatumisesta/varoitus_kaatumisesta.ino
+++ varoitus_kaatumisesta/varoitus_kaatumisesta.ino
@@ -1,27 +1,34 @@
-int asentokytkin = 0;
+// Varoitus kaatumisesta
+//
+// Tässä esimerkissä esitellään asentokytkimen ja piezo-kaiuttimen käyttöä.
+// Kun nalle kaadetaan, kaiuttimesta soitetaan hälytysajoneuvoa muistuttava ääni.
+//
+
+int asentokytkin = 0;  // Muuttuja asentokytkimen tilan tallentamiseen
 
 void setup() 
 {
-  pinMode(8, INPUT);
+  pinMode(8, INPUT);  // Asetetan pinni INPUT tilaan
 }
 
 void loop() 
 {
-  asentokytkin = digitalRead(8);
+  asentokytkin = digitalRead(8); // Luetaan asentokytkimen tila
   
-  delay(100);
+  delay(100);                    // Odotetaan 100 millisekuntia
   
-  if(asentokytkin == 0 ) {
+  if(asentokytkin == 0 ) {       // Mikäli asentokytkimen tila on 0 käynnistetään sireeni
+    // Seuraava toistolause käy läpi numerot 415:sta 614:sta yksi kerrallaan.
     for (int taajuus=415; taajuus <= 615; taajuus++) {
-      tone(12, taajuus);
-      delay(5);
+      tone(12, taajuus); // Soitetaan ääni, jonka 'taajuus' muuttuja sisältää
+      delay(5);          // Odotetaan 5 millisekuntia
     }
     for (int taajuus=615; taajuus > 415; taajuus--) {
-      tone(12, taajuus);
-      delay(5);
+      tone(12, taajuus); // Soitetaan ääni, jonka 'taajuus' muuttuja sisältää
+      delay(5);          // Odotetaan 5 millisekuntia
     }
   }
   else {
-    noTone(12);    
+    noTone(12); // Sammutetaan kaiutin
   }
 } 
