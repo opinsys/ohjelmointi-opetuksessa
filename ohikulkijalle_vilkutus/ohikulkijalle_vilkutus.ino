3e8c505eb91ab97945d79b22daf2efd93989c479
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2015-03-27 08:53:36 +0200

    Ohikulkijalle vilkutus: lyhyempi kalibrointi aika ja äänimerkki, kun kalibrointi on suoritettu

--- ohikulkijalle_vilkutus/ohikulkijalle_vilkutus.ino
+++ ohikulkijalle_vilkutus/ohikulkijalle_vilkutus.ino
@@ -9,13 +9,15 @@ void setup()
   tassu.attach(4);
   tassu.write(20);
 
-  while (millis() < 4000) {
+  while (millis() < 2000) {
     valovastuksen_arvo = analogRead(0);
     if (valovastuksen_arvo < matalin_valovastuksen_arvo) {
      matalin_valovastuksen_arvo = valovastuksen_arvo;
     }
   }
   matalin_valovastuksen_arvo = matalin_valovastuksen_arvo - 5;
+
+  tone(12, 440, 500);
 }
 
 void loop()
