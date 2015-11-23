7f6dca9667fdeef95ada563ce2aa8fd4af2dd2e0
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2015-11-23 22:16:31 +0200

    Sketchbook sijainti oletuksen mukaiseksi

--- zumo-32u4/install-Zumo32U4.sh
+++ zumo-32u4/install-Zumo32U4.sh
@@ -6,19 +6,19 @@ wget https://github.com/pololu/a-star/archive/master.zip
 unzip master.zip
 rm master.zip
 
-mkdir -p ~/arduino/hardware
-cp -r a-star-master/add-on/pololu/ ~/arduino/hardware
+mkdir -p ~/sketchbook/hardware
+cp -r a-star-master/add-on/pololu/ ~/sketchbook/hardware
 
 wget https://github.com/pololu/zumo-32u4-arduino-library/archive/master.zip
 unzip master.zip
 rm master.zip
 
-mkdir -p ~/arduino/libraries
+mkdir -p ~/sketchbook/libraries
 mv zumo-32u4-arduino-library-master Zumo32U4
-mv Zumo32U4/ ~/arduino/libraries/
+mv Zumo32U4/ ~/sketchbook/libraries/
 
 wget https://github.com/opinsys/ohjelmointi-opetuksessa/archive/master.zip
 unzip master.zip
 rm master.zip
-mkdir -p ~/arduino/libraries/Zumo32U4_opinsys/examples
-cp -r ohjelmointi-opetuksessa-master/zumo-32u4/* ~/arduino/libraries/Zumo32U4_opinsys/examples
+mkdir -p ~/sketchbook/libraries/Zumo32U4_opinsys/examples
+cp -r ohjelmointi-opetuksessa-master/zumo-32u4/* ~/sketchbook/libraries/Zumo32U4_opinsys/examples
