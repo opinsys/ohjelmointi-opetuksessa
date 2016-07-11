d949f2ab297721f7a57300607636c192415b5de9
ptoivola <ptoivola@users.noreply.github.com>
2016-07-11 12:08:48 +0300

    Update ohikulkijalle_vilkutus.ino

--- nalle/ohikulkijalle_vilkutus/ohikulkijalle_vilkutus.ino
+++ nalle/ohikulkijalle_vilkutus/ohikulkijalle_vilkutus.ino
@@ -1,13 +1,13 @@
 // Nalle heilauttaa tassua, kun valon määrä alittaa asetetun kynnyksen.
 // Kynnys säädetään ohjelman ensimmäisten sekuntien aikana senhetkisen valaistuksen perusteella.
 // Ohikulkijan luoma varjo aiheuttaa valon määrän vähenemisen. 
-#include <Servo.h>
+#include <Servo.h> // Sisällytä ohjelmaan kirjasto Servo.h 
 
 Servo tassu; // Luodaan servo-objekti tassun liikuttamiseksi.
 int valovastuksen_arvo; // Luodaan muuttuja valovastuksen arvon tallentamiseksi.
 int matalin_valovastuksen_arvo = 1023; // Luodaan muuttuja, joka on valaistuksen kynnys tassun heilauttamiseksi ja annetaan sille alkuarvo.
 
-void setup() // Tehdään asetukset servolle ja lasketaan 2 sekunnin aikana valaistuksen kynnys, jos se on oletusarvoa matalampi.
+void setup() // Tehdään asetukset servolle ja lasketaan 2 sekunnin aikana valaistuksen kynnystä, jos se on oletusarvoa matalampi.
 {
   tassu.attach(4); // Liitetään servo-objekti pinniin 4.
   tassu.write(20); // Käännetään servo asentoon 20 astetta.
@@ -16,7 +16,7 @@ void setup() // Tehdään asetukset servolle ja lasketaan 2 sekunnin aikana vala
     valovastuksen_arvo = analogRead(0); // tallennetaan valovastuksen muuttujan arvo yhtä suureksi kuin analogisen pinnin 0 arvo tällä hetkellä, ja
     if (valovastuksen_arvo < matalin_valovastuksen_arvo) { // jos valovastuksen muuttujan arvo on pienempi kuin kynnyksen muuttujan arvo,
      matalin_valovastuksen_arvo = valovastuksen_arvo; // kynnyksen muuttujan arvo lasketaan yhtä suureksi kuin valovastuksen muuttujan arvo.
-    }
+    } // if-ehtolause päättyy
   } // Palataan while-silmukan alkuun, jos sen ehto on totta.
   matalin_valovastuksen_arvo = matalin_valovastuksen_arvo - 5; // Lasketaan kynnyksen muuttujan arvoa viidellä reagointiherkkyyden vähentämiseksi.
 
@@ -31,17 +31,17 @@ void loop() // Toista ikuisesti:
     tassu.write(160); // käännä tassu asentoon 160 astetta ja
     delay(500); // odota 500 millisekuntia.
 
-    for(int i = 1; i <= 2; i++) { // Suorita kunnes muuttujan i, jonka alkuarvo on 1, on pienempi tai yhtäsuuri kuin 2 ja loopin lopuksi nosta i:n arvoa yhdellä.
+    for(int i = 1; i <= 2; i++) { // Suorita kunnes muuttujan i (alkuarvo=1) arvo on pienempi tai yhtäsuuri kuin 2. Silmukan lopussa nosta i:n arvoa yhdellä.
       tassu.write(120); // Käännä tassu asentoon 120 astetta ja 
       delay(150); // odota 150 millisekuntia.
 
       tassu.write(160); // Käännä tassu asentoon 160 astetta ja 
       delay(150); // odota 150 millisekuntia.
-    } // Nostetaan i:n arvoa yhdellä ja palataan for-silmukan alkuun.
+    } // Nostetaan i:n arvoa yhdellä ja palataan for-silmukan alkuun, jos sen ehto on edelleen totta.
 
     tassu.write(20); // Käännä tassu asentoon 20 astetta ja 
     delay(1000); // odota 1000 millisekuntia.
-  }
+  } // if-ehtolause päättyy
 
   delay(10); // Odota 10 millisekuntia. Tämä on viive valovastuksen arvon lukemiselle. Muuten arvoa yritettäisiin lukea äärettömän nopeasti.
 } // Palataan loop-silmukan alkuun.
